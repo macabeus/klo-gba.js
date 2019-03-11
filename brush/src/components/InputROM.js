9a0ebb9b9fb804166e5ed714e51b6d3af1936bf5
macabeus <bruno.macabeus@gmail.com>
2019-03-11 02:57:16 -0300

    brush: add InputROM component

---
+++ brush/src/components/InputROM.js
@@ -0,0 +1,36 @@
+import React, { useContext } from 'react'
+import { Button } from 'former-kit'
+import FileReaderInput from 'react-file-reader-input'
+import ROMContext from '../context/ROMContext'
+
+const getFileContent = async file =>
+  new Promise((resolve, reject) => {
+    const reader = new FileReader()
+
+    reader.onerror = (ev) => {
+      reject(ev.target.error)
+    }
+
+    reader.onload = (ev) => {
+      const buffer = ev.target.result
+      resolve(new Uint8Array(buffer))
+    }
+
+    reader.readAsArrayBuffer(file)
+  })
+
+const handleChange = setROMBuffer => async (event, [[, file]]) =>
+  (await getFileContent(file))
+  |> setROMBuffer
+
+const InputROM = () => {
+  const { setROMBuffer } = useContext(ROMContext)
+
+  return (
+    <FileReaderInput as="binary" onChange={handleChange(setROMBuffer)}>
+      <Button>Select the ROM file</Button>
+    </FileReaderInput>
+  )
+}
+
+export default InputROM
---
+++ brush/src/context/ROMContext.js
@@ -0,0 +1,6 @@
+import React from 'react'
+
+// Store the ROM loaded by the user
+const ROMContext = React.createContext()
+
+export default ROMContext
