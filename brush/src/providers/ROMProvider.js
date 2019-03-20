95c2452e06493996313499e7510398a87559ff00
macabeus <bruno.macabeus@gmail.com>
2019-03-19 23:16:15 -0300

    brush: add save and auto load rom using IndexedDB

--- brush/src/providers/ROMProvider.js
+++ brush/src/providers/ROMProvider.js
@@ -1,14 +1,47 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import PropTypes from 'prop-types'
+import localforage from 'localforage'
 import ROMContext from '../context/ROMContext'
 
+const useROMBuffer = () => {
+  const romBufferStates = {
+    empty: { memory: null, status: 'empty' },
+    loaded: memory => ({ memory, status: 'loaded' }),
+    starting: { memory: null, status: 'starting' },
+  }
+
+  const [romBufferState, setROMBufferState] = useState(romBufferStates.starting)
+
+  useEffect(() => {
+    const fetchLocalROMBufferMemory = async () => {
+      const romBufferMemory = await localforage.getItem('romBuffer')
+
+      if (romBufferMemory) {
+        setROMBufferState(romBufferStates.loaded(romBufferMemory))
+      } else {
+        setROMBufferState(romBufferStates.empty)
+      }
+    }
+
+    fetchLocalROMBufferMemory()
+  })
+
+  const updateROMBufferState = (newRomBufferMemory) => {
+    setROMBufferState(romBufferStates.loaded(newRomBufferMemory))
+    localforage.setItem('romBuffer', newRomBufferMemory)
+  }
+
+  return [romBufferState, updateROMBufferState]
+}
+
 const ROMProvider = (props) => {
-  const [romBuffer, setROMBuffer] = useState(null)
+  const [romBuffer, setROMBuffer] = useROMBuffer()
 
   return (
     <ROMContext.Provider
       value={{
-        romBuffer,
+        romBufferMemory: romBuffer.memory,
+        romBufferStatus: romBuffer.status,
         setROMBuffer,
       }}
     >
