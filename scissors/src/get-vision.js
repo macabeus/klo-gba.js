8e1544affab8f7fc855eceeb1b33bc65066a1515
macabeus <bruno.macabeus@gmail.com>
2019-03-24 02:47:14 -0300

    brush+scissors: refactor moving oam binary parse from brush to scissors

--- scissors/src/get-vision.js
+++ scissors/src/get-vision.js
@@ -1,4 +1,5 @@
-import { drop, splitEvery } from 'ramda'
+import { drop, splitEvery, map, filter } from 'ramda'
+import binary from 'binary'
 import huffmanDecode from './huffman'
 import lzssDecode from './lzss'
 
@@ -11,6 +12,23 @@ const extractTilemap = (romBuffer, [addressStart, addressEnd]) =>
 const extractOAM = (romBuffer, [addressStart, addressEnd]) =>
   romBuffer.slice(addressStart, addressEnd)
   |> splitEvery(44)
+  |> map(memory => ({
+    data: binary.parse(memory)
+      .word16lu('xStage1')
+      .word16lu('yStage1')
+      .skip(4)
+      .word16lu('xStage2')
+      .word16lu('yStage2')
+      .skip(4)
+      .word16lu('xStage3')
+      .word16lu('yStage3')
+      .skip(21)
+      .word8lu('kind')
+      .vars,
+    memory,
+  }))
+  |> filter(({ data }) =>
+    data.kind !== null)
 
 const getVision = (romBuffer, world, vision) => {
   const infos = require(`./visions/${world}-${vision}.js`).default // eslint-disable-line
