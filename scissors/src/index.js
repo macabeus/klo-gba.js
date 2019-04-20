ae775dfa98e6c84330b9af8cccb0941bfb7051b1
macabeus <bruno.macabeus@gmail.com>
2019-04-19 23:54:45 -0300

    brush+scissors: refactor moving func to get tile name to scissors

--- scissors/src/index.js
+++ scissors/src/index.js
@@ -1,7 +1,10 @@
 import getVision from './get-vision'
 import { oamIdToName } from './oam-maps'
+import fromSchemeGetTileNameById from './from-scheme-get-tile-name-by-id'
 
 export default {
   getVision,
   oamIdToName,
 }
+
+export { fromSchemeGetTileNameById }
