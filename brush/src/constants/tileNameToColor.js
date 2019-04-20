32f1f2de01f1f02ccdabd232eb3467790e357b94
macabeus <bruno.macabeus@gmail.com>
2019-04-19 23:40:17 -0300

    brush: refactor creating a file to store the map tile name to its color

---
+++ brush/src/constants/tileNameToColor.js
@@ -0,0 +1,15 @@
+const tileNameToColor = {
+  board: '199, 199, 199, 1',
+  bridge: '200, 131, 63, 1',
+  bridgeRope: '200, 131, 63, 0.5',
+  darkRock: '90, 60, 63, 1',
+  empty: '255, 255, 255, 0',
+  grass: '0, 125, 0, 1',
+  lightRock: '200, 100, 63, 1',
+  rock: '160, 60, 63, 1',
+  spike: '200, 20, 70, 1',
+  unknown: '0, 0, 0, 1',
+  wood: '188, 111, 93, 1',
+}
+
+export default tileNameToColor
