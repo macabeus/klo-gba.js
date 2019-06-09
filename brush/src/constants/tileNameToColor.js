62555d8c14e5cf133081829be7171606e98f3fc2
macabeus <bruno.macabeus@gmail.com>
2019-06-08 22:04:42 -0300

    vision: add 1-3

--- brush/src/constants/tileNameToColor.js
+++ brush/src/constants/tileNameToColor.js
@@ -1,4 +1,5 @@
 const tileNameToColor = {
+  blueDoor: '100, 100, 215, 0.5',
   board: '199, 199, 199, 1',
   bridge: '200, 131, 63, 1',
   bridgeRope: '200, 131, 63, 0.5',
@@ -6,6 +7,7 @@ const tileNameToColor = {
   empty: '255, 255, 255, 0',
   grass: '0, 125, 0, 1',
   lightRock: '200, 100, 63, 1',
+  redDoor: '215, 100, 100, 0.5',
   rock: '160, 60, 63, 1',
   spike: '200, 20, 70, 1',
   unknown: '0, 0, 0, 1',
