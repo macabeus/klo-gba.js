0cd846aa695e20974a909bbee4666653e2888941
macabeus <bruno.macabeus@gmail.com>
2019-06-22 11:57:53 -0300

    icon: fix display on built website

--- brush/config/webpack.config.js
+++ brush/config/webpack.config.js
@@ -4,6 +4,7 @@ const CopyPlugin = require('copy-webpack-plugin')
 const formerKitRules = require('./former-kit/webpack.config')
 
 const htmlPlugin = new HtmlWebPackPlugin({
+  favicon: './assets/Klonoa-icon.ico',
   filename: './index.html',
   template: './src/index.html',
 })
