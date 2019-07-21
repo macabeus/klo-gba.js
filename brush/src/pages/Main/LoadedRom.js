f57a5eef3df6f901c2a71dc2b8399e08962e5f71
macabeus <bruno.macabeus@gmail.com>
2019-07-21 16:17:04 -0300

    display-map-options: refactor moving its code to a new component

--- brush/src/pages/Main/LoadedRom.js
+++ brush/src/pages/Main/LoadedRom.js
@@ -1,17 +1,14 @@
 import React, { useContext, useState, Fragment } from 'react'
 import {
-  Button,
   Card,
   CardContent,
   CardTitle,
-  Checkbox,
   Flexbox,
-  Popover,
-  PopoverContent,
   Spacing,
 } from 'former-kit'
 import ObjectsTable from '../../components/ObjectsTable'
 import VisionContext from '../../context/VisionContext'
+import DisplayMapOptios from '../../components/DisplayMapOptions'
 import Map from '../../components/Map'
 import MapEmptyState from '../../components/MapEmptyState'
 import SaveButton from '../../components/MapActionsBar/SaveButton'
@@ -50,37 +47,14 @@ const LoadedRom = () => {
             <Spacing size="tiny" />
             <SaveButton />
             <Spacing size="tiny" />
-            <Popover
-              content={
-                <PopoverContent>
-                  <Fragment>
-                    <Checkbox
-                      label="Show grid"
-                      name="optShowGrid"
-                      value={`${optShowGrid}`}
-                      checked={optShowGrid}
-                      onChange={() => { setOptShowGrid(!optShowGrid) }}
-                    />
-                    <Checkbox
-                      label="Show OAM"
-                      name="optShowOAM"
-                      value={`${optShowOAM}`}
-                      checked={optShowOAM}
-                      onChange={() => { setOptShowOAM(!optShowOAM) }}
-                    />
-                    <Checkbox
-                      label="Show Portals"
-                      name="optShowPortals"
-                      value={`${optShowPortals}`}
-                      checked={optShowPortals}
-                      onChange={() => { setOptShowPortals(!optShowPortals) }}
-                    />
-                  </Fragment>
-                </PopoverContent>
-              }
-            >
-              <Button>Display map options</Button>
-            </Popover>
+            <DisplayMapOptios
+              optShowGrid={optShowGrid}
+              optShowOAM={optShowOAM}
+              optShowPortals={optShowPortals}
+              onChangeOptShowGrid={setOptShowGrid}
+              onChangeOptShowOAM={setOptShowOAM}
+              onChangeOptShowPortals={setOptShowPortals}
+            />
           </Flexbox>
 
           <Spacing />
