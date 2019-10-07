956a916a1825752ca79fcafa4bbfe82e6ad3b05d
macabeus <bruno.macabeus@gmail.com>
2019-10-07 23:01:42 +0100

    klo-gba-sidebar: add sidebar
    At this first moment, KloGbaSidebar only have the logo

---
+++ brush/src/components/KloGbaSidebar/SidebarLogo/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import { SidebarHeader } from 'former-kit'
+import Logo from '../../../../assets/Klo-GBA_JS_Logotipo.svg'
+
+const SidebarLogo = () => (
+  <SidebarHeader>
+    <Logo style={{ height: '45px' }} />
+  </SidebarHeader>
+)
+
+export default SidebarLogo
