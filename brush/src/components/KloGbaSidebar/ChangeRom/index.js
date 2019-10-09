a9f34d2db258b25cfbbc74f394e5c9456af0aeac
macabeus <bruno.macabeus@gmail.com>
2019-10-09 20:27:44 +0100

    klo-gba-sidebar: move ChangeRom button to inside it

--- brush/src/components/KloGbaSidebar/ChangeRom/index.js
+++ brush/src/components/KloGbaSidebar/ChangeRom/index.js
@@ -1,21 +1,20 @@
 import React, { useContext } from 'react'
-import { Button } from 'former-kit'
-import ROMContext from '../../context/ROMContext'
-import VisionContext from '../../context/VisionContext'
+import { SidebarLink } from 'former-kit'
+import ROMContext from '../../../context/ROMContext'
+import VisionContext from '../../../context/VisionContext'
 
 const ChangeRom = () => {
   const { setROMBuffer } = useContext(ROMContext)
   const { setEmptyState } = useContext(VisionContext)
 
   return (
-    <Button
+    <SidebarLink
+      title="Change ROM"
       onClick={() => {
         setROMBuffer(null)
         setEmptyState()
       }}
-    >
-      Change ROM
-    </Button>
+    />
   )
 }
 
