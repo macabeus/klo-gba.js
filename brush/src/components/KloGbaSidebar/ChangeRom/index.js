48d901018544af4574f507f15d8ea0793757b2c1
macabeus <bruno.macabeus@gmail.com>
2019-10-12 19:58:11 +0100

    input-rom: add cancel button

--- brush/src/components/KloGbaSidebar/ChangeRom/index.js
+++ brush/src/components/KloGbaSidebar/ChangeRom/index.js
@@ -4,14 +4,14 @@ import ROMContext from '../../../context/ROMContext'
 import VisionContext from '../../../context/VisionContext'
 
 const ChangeRom = () => {
-  const { setROMBuffer } = useContext(ROMContext)
+  const { putOnHoldROMBufferState } = useContext(ROMContext)
   const { setEmptyState } = useContext(VisionContext)
 
   return (
     <SidebarLink
       title="Change ROM"
       onClick={() => {
-        setROMBuffer(null)
+        putOnHoldROMBufferState()
         setEmptyState()
       }}
     />
