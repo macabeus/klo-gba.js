f16967d12c64ff4301a79bca085dc8f2554bb720
macabeus <bruno.macabeus@gmail.com>
2019-11-06 23:04:06 +0000

    object-maps: change names to a more friendly one

--- scissors/src/objectMaps.js
+++ scissors/src/objectMaps.js
@@ -1,16 +1,16 @@
 const objectIdToName = {
-  0x01: 'redKey',
-  0x02: 'blueKey',
-  0x03: 'star',
-  0x05: 'door',
-  0x07: 'heart',
-  0x2C: 'dreamStone',
-  0x2D: 'dreamStoneLarge',
-  0x2E: 'oneUp',
-  0x6F: 'box',
-  0x76: 'moo',
-  0x77: 'flyingMooHorizontal',
-  0x78: 'flyingMooVerical',
+  0x01: 'Red Key',
+  0x02: 'Blue Key',
+  0x03: 'Star',
+  0x05: 'Door',
+  0x07: 'Heart',
+  0x2C: 'Dream Stone',
+  0x2D: 'Large Dream Stone',
+  0x2E: 'One Up',
+  0x6F: 'Box',
+  0x76: 'Moo',
+  0x77: 'Flying Moo ↔',
+  0x78: 'Flying Moo ↕',
 }
 
 export { objectIdToName } // eslint-disable-line import/prefer-default-export
