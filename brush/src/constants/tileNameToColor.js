3068de3eb401dce6ea5590f2ad613b8ac2ac9718
macabeus <bruno.macabeus@gmail.com>
2020-01-02 00:43:03 +0000

    visions: add vision 1-5

--- brush/src/constants/tileNameToColor.js
+++ brush/src/constants/tileNameToColor.js
@@ -1,17 +1,24 @@
 const tileNameToColor = {
   blueDoor: [0x6464D7, 0.5],
+  blueRock: [0x4F37BD, 1],
   board: [0xC7C7C7, 0.5],
   bridge: [0xC8833F, 1],
   bridgeRope: [0xC8833F, 0.5],
+  bush: [0x004700, 0.5],
+  circleWood: [0xD47B68, 1],
+  column: [0xBA6F3D, 0.5],
   darkRock: [0x5A3C3F, 1],
   empty: [0xFFFFFF, 1],
   grass: [0x007D00, 1],
+  ladder: [0x46A54B, 0.75],
   lightRock: [0xC8643F, 1],
   redDoor: [0xD76464, 0.5],
   rock: [0xA03C3F, 1],
+  rope: [0xE8451A, 0.75],
   spike: [0xC81446, 1],
   unknown: [0x000000, 1],
   wood: [0xBC6F5D, 1],
+  wood2: [0xA66151, 1],
 }
 
 export default tileNameToColor
