9234688ccfcbe2dafcc8df330e29b929e167ec53
macabeus <bruno.macabeus@gmail.com>
2021-06-18 23:03:24 +0100

    chore: bump react-gbajs to 1.0.0

--- brush/src/components/Emulator/index.js
+++ brush/src/components/Emulator/index.js
@@ -1,23 +1,44 @@
-import React, { useEffect, useContext } from 'react'
+import React, { useEffect, useContext, useState } from 'react'
 import GBAEmulator, { GbaContext } from 'react-gbajs'
 import useGbaSaveRestoreState from '../../hooks/useGbaSaveRestoreState'
 import ROMContext from '../../context/ROMContext'
 
 const Emulator = () => {
+  const [onError, setOnError] = useState(false)
   const { play: playGba } = useContext(GbaContext)
   const { romBufferMemory, romBufferStatus } = useContext(ROMContext)
 
   useEffect(() => {
+    setOnError(false)
+
     if (romBufferStatus === 'loaded') {
-      playGba(romBufferMemory)
+      playGba({ newRomBuffer: romBufferMemory })
     }
   }, [romBufferStatus]) /* should not be called when romBufferState.memory updates */ // eslint-disable-line react-hooks/exhaustive-deps
 
   useGbaSaveRestoreState()
 
+  const handleEmulatorCrashed = (...error) => {
+    setOnError(true)
+
+    // eslint-disable-next-line no-console
+    console.warn(error)
+  }
+
+  if (onError) {
+    return (
+      <span>
+        Emulator crashed. Please, open a new ROM or refresh this page.
+      </span>
+    )
+  }
+
   return (
     <>
-      <GBAEmulator volume={0} />
+      <GBAEmulator
+        scale={2}
+        onLogReceived={handleEmulatorCrashed}
+      />
       <span>
         Controls:{' '}
         <strong>z</strong> → a;{' '}
